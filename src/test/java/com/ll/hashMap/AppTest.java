67da35446b97c841ee6f35956d0aa15fde550fd7
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-18 19:38:53 +0900

    제네릭 - 다양한 타입(클래스) 넣어도 작동하는지 test.

--- src/test/java/com/ll/hashMap/AppTest.java
+++ src/test/java/com/ll/hashMap/AppTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -78,10 +79,51 @@ class HashMapTest {
         assertEquals(true, keySet.contains("영희"));
         assertEquals(false, keySet.contains("영수"));
     }
+    @Test
+    void 원숭이_맵() {
+        HashMap<String, 원숭이> monkeys = new HashMap<>();
+
+        원숭이 a원숭이_키키 = new 원숭이("키키");
+        원숭이 a원숭이_코코 = new 원숭이("코코");
+
+        monkeys.put("키키", a원숭이_키키);
+        monkeys.put("코코", a원숭이_코코);
+
+        assertEquals(monkeys.get("키키"), a원숭이_키키);
+        assertEquals(monkeys.get("코코"), a원숭이_코코);
+    }
+    @Test
+    void 다양한_종류의_객체들의_맵() {
+        HashMap<String, Object> monkeys = new HashMap<>();
+
+        monkeys.put("원숭이_키키", new 원숭이("키키"));
+        monkeys.put("사람_존", new 사람("존"));
+
+        원숭이 a원숭이_키키 = (원숭이) monkeys.get("원숭이_키키");
+        사람 a사람_존 = (사람) monkeys.get("사람_존");
+
+        a원숭이_키키.묘기를_부리다();
+        a사람_존.말하다();
+    }
 
 
+}
+class 사람 {
+    private String name;
+    public 사람(String name) {
+        this.name = name;
+    }
+    void 말하다() {
+        System.out.println("사람이 말합니다.");
+    }
+}
 
-
-
-
+class 원숭이 {
+    private String name;
+    public 원숭이(String name) {
+        this.name = name;
+    }
+    void 묘기를_부리다() {
+        System.out.println("원숭이가 묘기를 부립니다.");
+    }
 }
