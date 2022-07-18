caf71af78fda74fcf7b7cd405e7b182617ccfed4
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-07-18 14:35:17 +0900

    데이터 수정까지 구현 오류 fix - put 메서드에서 values가 아닌 keys에 값 삽입함.

--- src/main/java/com/ll/hashMap/HashMap.java
+++ src/main/java/com/ll/hashMap/HashMap.java
@@ -1,4 +1,52 @@
 package com.ll.hashMap;
 
-public class HashMap {
+public class HashMap<K, V> {
+    Object[] keys;
+    Object[] values;
+    int size;
+
+    public HashMap() {
+        keys = new Object[100];
+        values = new Object[100];
+        size = 0;
+    }
+
+    public void put(K key, V value) {
+        if (isSameKey(key, value)) {
+            int index = getIndexOfKey(key);
+            if( index != -1 ) values[index] = value;
+            return;
+        }
+        size++;
+        keys[size-1] = key;
+        values[size-1] = value;
+
+
+    }
+
+    public V get(K key) {
+        int index = getIndexOfKey(key);
+        if(index == -1) return null;
+        return (V)values[index];
+    }
+
+    public int getIndexOfKey(K key) {
+        for (int i = 0; i < size; i++) {
+            if(keys[i].equals(key)) {
+                return i;
+            }
+        }
+        return -1; // 없는 경우.
+    }
+
+    public boolean isSameKey(K key, V value) {
+        for (int i = 0; i < size; i++) {
+            if (keys[i].equals(key)) {
+                values[i] = value;
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/test/java/com/ll/hashMap/AppTest.java
+++ src/test/java/com/ll/hashMap/AppTest.java
@@ -2,6 +2,8 @@ package com.ll.hashMap;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AppTest {
 }
 
@@ -11,6 +13,40 @@ class HashMapTest {
         HashMap map = new HashMap();
     }
 
+    @Test
+    void 제너릭이_가능하다() {
+        HashMap<String, Integer> map = new HashMap<>();
+    }
+    @Test
+    void put() {
+        HashMap<String, Integer> ages = new HashMap<>();
+        ages.put("철수", 22);
+    }
+    @Test
+    void get() {
+        HashMap<String, Integer> ages = new HashMap<>();
+        ages.put("철수", 22);
+        int age = ages.get("철수");
+
+        assertEquals(22, age);
+    }
+    @Test
+    void _2nd_get() {
+        HashMap<String, Integer> ages = new HashMap<>();
+        ages.put("철수", 22);
+        ages.put("영희", 25);
+        assertEquals(22, ages.get("철수"));
+        assertEquals(25, ages.get("영희"));
+    }
+    @Test
+    void put__데이터_수정() {
+        HashMap<String, Integer> ages = new HashMap<>();
+        ages.put("철수", 22);
+        ages.put("영희", 25);
+        ages.put("영희", 27);
+
+        assertEquals(27, ages.get("영희"));
+    }
 
 
 }
